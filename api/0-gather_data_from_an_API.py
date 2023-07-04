373ca5354caa4a85e44d601b9e6b78e899a54732
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2023-07-04 18:34:20 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python3
-
-"""Import libraries"""
+""" Library to gather data from an API """
 
 import requests
 import sys
 
-
-"""Import libraries"""
+""" Function to gather data from an API """
 
 if __name__ == "__main__":
-    user_id = int(sys.argv[1])
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
 
-    todo_data = requests.get(todos_url).json()
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
 
-    employee_name = requests.get(users_url).json()["name"]
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
 
-    total_user_todos = 0
-    completed_todos = 0
-    titles = []
-    for todo in todo_data:
-        # Check if user_id is the same input as parameter
-        if user_id == todo["userId"]:
-            # Get the total number of todos
-            total_user_todos += 1
-            # Get the total number of completed tasks
-            if todo["completed"]:
-                completed_todos += 1
-                # Get the titles of the completed tasks
-                titles.append(todo["title"])
+    employee_name = user_info.get("name")
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_com = len(total_tasks)
+    total_task_done = len(todo_info)
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, completed_todos, total_user_todos))
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          task_com, total_task_done))
 
-    for title in titles:
-        print("\t {}".format(title))
+    [print("\t {}".format(task.get("title"))) for task in total_tasks]
